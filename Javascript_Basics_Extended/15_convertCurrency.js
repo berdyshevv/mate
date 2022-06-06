3342cb85386e25456a7d3e7172ce0f25b4659b7e
“vitia” <viktorberdysh@gmail.com>
2022-06-06 18:01:30 +0200

    1 problem fix

--- Javascript_Basics_Extended/15_convertCurrency.js
+++ Javascript_Basics_Extended/15_convertCurrency.js
@@ -6,14 +6,14 @@
  * @returns {string}
  */
  function convertCurrency(amount, exchangeRate, currencyName) {
+    if (amount <= 0 || exchangeRate <= 0) {
+        return 'Enter valid data';
+    } 
+
     const exchangedAmount = amount * exchangeRate;
     const mulAmount = exchangedAmount * 100;
     const rMAmount = Math.floor(mulAmount);
     const rAmount = rMAmount / 100;
-  
-    if (amount <= 0 || exchangeRate <= 0) {
-      return 'Enter valid data';
-    }
   
     return 'Give them ' + rAmount + ' ' + currencyName + '(\'s)';
   }
